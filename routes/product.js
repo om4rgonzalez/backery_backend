06d359a2b6682318d239595fce2e282df6a5450f
Marcelo Gonzalez <om4r.gonzalez@gmail.com>
2020-06-02 09:24:44 -0300

    Creando ruta de productos

---
+++ routes/product.js
@@ -0,0 +1,8 @@
+const bodyParser = require('body-parser'); //bodyParser nos permite reicibir parametros por POST
+const { Router } = require('express');
+const router = Router();
+
+
+
+
+module.exports = router;
